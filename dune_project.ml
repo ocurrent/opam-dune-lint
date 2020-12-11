051d62600bce5ef9b19651d781ea0ff92eec3249
Thomas Leonard <talex5@gmail.com>
2020-12-11 14:58:24 +0000

    Use "dune describe" to find internal libraries
    Avoids warning about missing internal dependencies.
    
    Also, skip dependencies of vendored projects; assume that the vendored
    projects themselves will handle their dependencies correctly.
    
    Note that this assumes we're using vendoring as a way of pinning
    unreleased software. It's possible that people might also use vendoring
    as a way to include a library privately. In that case, this isn't what
    is wanted and we might need to add an option later.

--- dune_project.ml
+++ dune_project.ml
@@ -6,6 +6,10 @@ let atom = Dune_lang.atom
 let dune_and x y =  Dune_lang.(List [atom "and"; x; y])
 let lower_bound v = Dune_lang.(List [atom ">="; atom (OpamPackage.Version.to_string v)])
 
+let or_die = function
+  | Ok x -> x
+  | Error (`Msg m) -> failwith m
+
 let parse () =
   Stdune.Path.Build.(set_build_dir (Kind.of_string (Sys.getcwd ())));
   let path = Stdune.Path.of_string "dune-project" in
@@ -97,3 +101,117 @@ let write_project_file t =
   Fmt.pf f "@[<v>%a@]@." (Fmt.list ~sep:Fmt.cut (Fmt.using Dune_lang.pp Stdune.Pp.render_ignore_tags)) t;
   close_out ch;
   Fmt.pr "Wrote %S@." path
+
+module Deps = struct
+  (*  We use [tmp_dir] so that "--only-packages" doesn't invalidate the existing build. *)
+  let dune_external_lib_deps ~tmp_dir ~pkg ~target =
+    let tmp_dir = Fpath.to_string tmp_dir in
+    Bos.Cmd.(v "dune" % "external-lib-deps" % "--only-packages" % pkg
+             % "--build-dir" % tmp_dir
+             % "--sexp" % "--unstable-by-dir"
+             % target)
+
+  let has_dune_subproject = function
+    | "." | "" -> false
+    | dir -> Sys.file_exists (Filename.concat dir "dune-project")
+
+  let rec should_use_dir ~dir_types path =
+    match Hashtbl.find_opt dir_types path with
+    | Some x -> x
+    | None ->
+      let r =
+        match Astring.String.cut ~sep:"/" ~rev:true path with
+        | Some (parent, _) ->
+          if should_use_dir ~dir_types parent then (
+            not (has_dune_subproject path)
+          ) else false
+        | None ->
+          not (has_dune_subproject path)
+      in
+      Hashtbl.add dir_types path r;
+      r
+
+  let merge_dep acc = function
+    | Sexplib.Sexp.List (Atom lib :: _) -> Libraries.add lib acc
+    | x -> Fmt.failwith "Bad output from 'dune external-lib-deps': %a" Sexplib.Sexp.pp_hum x
+
+  let merge_dir ~dir_types acc = function
+    | Sexplib.Sexp.List [Atom path; List deps] ->
+      if should_use_dir ~dir_types path then (
+        (* Fmt.pr "Process %S@." path; *)
+        List.fold_left merge_dep acc deps
+      ) else (
+        (* Fmt.pr "Skip %S@." path; *)
+        acc
+      )
+    | x -> Fmt.failwith "Bad output from 'dune external-lib-deps': %a" Sexplib.Sexp.pp_hum x
+
+  let parse = function
+    | Sexplib.Sexp.List [Atom _ctx; List dirs] ->
+      let dir_types = Hashtbl.create 10 in
+      List.fold_left (merge_dir ~dir_types) Libraries.empty dirs
+    | x -> Fmt.failwith "Bad output from 'dune external-lib-deps': %a" Sexplib.Sexp.pp_hum x
+
+  (* Get the ocamlfind dependencies of [pkg]. *)
+  let get_external_lib_deps ~pkg ~target =
+    Bos.OS.Dir.with_tmp "dune-opam-lint-%s" (fun tmp_dir () ->
+        Bos.OS.Cmd.run_out (dune_external_lib_deps ~tmp_dir ~pkg ~target)
+        |> Bos.OS.Cmd.to_string
+        |> or_die
+      ) ()
+    |> or_die
+    |> String.trim
+    |> function
+    | "" -> Libraries.empty
+    | sexp -> parse (Sexplib.Sexp.of_string sexp)
+end
+
+module Csexp = struct
+  type t =
+    | Atom of string
+    | List of t list
+end
+
+module Sexp = Dune_csexp.Csexp.Make(Csexp)
+module Library_map = Map.Make(String)
+
+open Csexp
+
+type index = [`Internal | `External] Library_map.t
+
+let rec field name = function
+  | [] -> Fmt.failwith "Field %S is missing!" name
+  | List [Atom n; v] :: _ when n = name -> v
+  | _ :: xs -> field name xs
+
+let field_atom name xs =
+  match field name xs with
+  | Atom a -> a
+  | List _ -> Fmt.failwith "Expected %S to be an atom!" name
+
+let field_bool name xs =
+  bool_of_string (field_atom name xs)
+
+let index_lib acc fields =
+  let name = field_atom "name" fields in
+  let local = if field_bool "local" fields then `Internal else `External in
+  Library_map.add name local acc
+
+let index_item acc = function
+  | List [Atom "library"; List fields] -> index_lib acc fields
+  | _ -> acc
+
+let make_index = function
+  | List libs -> List.fold_left index_item Library_map.empty libs
+  | Atom _ -> failwith "Bad 'dune describe' output!"
+
+let describe () =
+  Bos.OS.Cmd.run_out (Bos.Cmd.(v "dune" % "describe" % "--format=csexp" % "--lang=0.1"))
+  |> Bos.OS.Cmd.to_string
+  |> or_die
+  |> Sexp.parse_string
+  |> function
+  | Error (_, e) -> Fmt.failwith "Error parsing 'dune describe' output: %s" e
+  | Ok x -> make_index x
+
+let lookup = Library_map.find_opt
---
+++ dune_project.mli
@@ -0,0 +1,25 @@
+open Types
+
+type t
+
+val parse : unit -> t
+(** [parse ()] loads the "dune-project" file. *)
+
+val generate_opam_enabled : t -> bool
+(** Check whether (generate_opam_files true) is present. *)
+
+val update : (_ * Change.t list) Paths.t -> t -> t
+
+val write_project_file : t -> unit
+
+type index
+
+val describe : unit -> index
+(** Create an index of the project's libraries, using "dune describe". *)
+
+val lookup : string -> index -> [`Internal | `External] option
+(** [lookup lib index] returns information from "dune describe" about [lib]. *)
+
+module Deps : sig
+  val get_external_lib_deps : pkg:string -> target:string -> Libraries.t
+end
