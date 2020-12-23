25d5761b044b9631259bc3ba5310ad39ee2dc9a7
Thomas Leonard <talex5@gmail.com>
2020-12-23 14:57:43 +0000

    Add bytes to the list of libraries supplied by OCaml

--- main.ml
+++ main.ml
@@ -17,7 +17,8 @@ let get_libraries ~pkg ~target =
 let to_opam ~index lib =
   match Astring.String.take ~sat:((<>) '.') lib with
   | "threads" | "unix" | "str" | "compiler-libs"
-  | "bigarray" | "dynlink" | "ocamldoc" | "stdlib" -> None          (* Distributed with OCaml *)
+  | "bigarray" | "dynlink" | "ocamldoc" | "stdlib"
+  | "bytes" -> None          (* Distributed with OCaml *)
   | lib ->
     match Index.Owner.find_opt lib index with
     | Some pkg -> Some pkg
