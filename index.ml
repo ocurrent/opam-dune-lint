b9be91519682c073418ea6cb629f8c75a296259c
David Allsopp <david.allsopp@metastack.com>
2021-07-02 20:54:27 +0100

    Restore compatibility with opam-state 2.0.x

--- index.ml
+++ index.ml
@@ -16,10 +16,10 @@ let update_index t changes ~pkg =
 
 let create () =
   let root = OpamStateConfig.opamroot () in
-  ignore (OpamStateConfig.load_defaults root);
+  ignore (OpamStateConfig.load_defaults ~lock_kind:`Lock_read root);
   OpamGlobalState.with_ `Lock_none @@ fun gt ->
   let switch = OpamStateConfig.get_switch () in
-  let installed = (OpamSwitchState.load_selections gt switch).sel_installed in
+  let installed = (OpamSwitchState.load_selections ~lock_kind:`Lock_read gt switch).sel_installed in
   OpamPackage.Set.fold (fun pkg acc ->
       let changes = OpamPath.Switch.changes gt.root switch (OpamPackage.name pkg) in
       match OpamFile.Changes.read_opt changes with
